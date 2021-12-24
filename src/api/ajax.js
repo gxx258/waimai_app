1cd32e91250efa041fae059b5a2ec78b23dfb9a2
gxx258 <2802449304@qq.com>
2021-12-24 11:58:30 +0800

    main

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -2,7 +2,7 @@
 ajax请求函数模块
 返回值: promise对象(异步返回的数据是: response.data)
  */
-import axios from 'axios'
+import axios from 'a  xios'
 export default function ajax (url, data={}, type='GET') {
 
   return new Promise(function (resolve, reject) {
